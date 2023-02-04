7f2614cbaf545004220ebdbe8ce98c01c759a931
Diego Ramos <diegorbrito9@gmail.com>
2023-02-04 13:00:03 -0300

    [backend] criando o primeiro teste para a rote '/' do app e mudando a extensão do arquivo de tipagem da entidade de 'Reserve'

---
+++ backend/test/hello-world.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+
+import agent from "supertest";
+
+import { app } from "../src/app";
+
+test("Should return a hello world message", async () => {
+  const expectBodyResponse = { message: "Hello world !" };
+
+  await app.ready();
+  const { body } = await agent(app.server).get("/").expect(200);
+
+  expect(body).toEqual(expectBodyResponse);
+});
