9c7b9746331a2e41eee6a183cfe2558c74ad2ded
Diego Ramos <diegorbrito9@gmail.com>
2023-02-06 10:13:41 -0300

    [backend] criando o primeiro teste, para a rota [GET] '/'

--- backend/test/hello-world.test.ts
+++ backend/test/hello-world.test.ts
@@ -1,14 +1,16 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import agent from "supertest";
 
 import { app } from "../src/app";
 
-test("Should return a hello world message", async () => {
-  const expectBodyResponse = { message: "Hello world !" };
+describe("Hello world", () => {
+  test("Should return a hello world message", async () => {
+    const expectBodyResponse = { message: "Hello world !" };
 
-  await app.ready();
-  const { body } = await agent(app.server).get("/").expect(200);
+    await app.ready();
+    const { body } = await agent(app.server).get("/").expect(200);
 
-  expect(body).toEqual(expectBodyResponse);
+    expect(body).toEqual(expectBodyResponse);
+  });
 });
